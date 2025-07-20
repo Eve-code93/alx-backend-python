ee8f826b55b7d2c462ce9bf016a1d02d47306364
Eve-code93 <evelynejose1993@gmail.com>
2025-07-20 20:32:59 +0300

    update tets utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,8 @@ from utils import access_nested_map
 
 from utils import get_json
 from unittest.mock import patch, Mock
+from utils import memoize
+from unittest.mock import patch
 
 class TestAccessNestedMap(unittest.TestCase):
     """Unit tests for access_nested_map function"""
@@ -41,3 +43,26 @@ class TestGetJson(unittest.TestCase):
             result = get_json(test_url)
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+class TestMemoize(unittest.TestCase):
+    """Unit tests for utils.memoize decorator"""
+
+    def test_memoize(self):
+        """Test that memoize caches method output"""
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            obj = TestClass()
+            result1 = obj.a_property
+            result2 = obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
