76f9799c0fa0f305e368d5ca2d8785827032716a
Eve-code93 <evelynejose1993@gmail.com>
2025-07-20 20:29:08 +0300

    Fix: add shebang to test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,8 @@
-from utils import access_nested_map
+#!/usr/bin/env python3
+"""Unit tests for utils.access_nested_map"""
 import unittest
 from parameterized import parameterized
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -15,10 +17,10 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'"),
     ])
-    def test_access_nested_map_exception(self, nested_map, path):
+    def test_access_nested_map_exception(self, nested_map, path, expected_message):
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(cm.exception), f"'{path[len(nested_map)]}'" if nested_map else "'a'")
+        self.assertEqual(str(cm.exception), expected_message)
