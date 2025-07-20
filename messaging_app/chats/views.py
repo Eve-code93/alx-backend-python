7518927a230e84ab05037f6745954d44955fbedf
Eve-code93 <evelynejose1993@gmail.com>
2025-07-20 22:26:27 +0300

    fix views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,31 +1,61 @@
-# chats/views.py
+from rest_framework import viewsets, filters, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
-from rest_framework import viewsets, filters
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
-from rest_framework.permissions import IsAuthenticated
+
 
 class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [filters.SearchFilter]
     search_fields = ['participants__username']
 
     def get_queryset(self):
-        return Conversation.objects.filter(participants=self.request.user)
+        return Conversation.objects.filter(participants=self.request.user).distinct()
 
     def perform_create(self, serializer):
         conversation = serializer.save()
         conversation.participants.add(self.request.user)
+        conversation.save()
+
+    def create(self, request, *args, **kwargs):
+        """Custom create to return 201 response with full data"""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['timestamp']
     ordering = ['timestamp']
+
+    def get_queryset(self):
+        queryset = Message.objects.filter(conversation__participants=self.request.user)
+        conversation_id = self.request.query_params.get('conversation')
+        if conversation_id:
+            queryset = queryset.filter(conversation_id=conversation_id)
+        return queryset.distinct()
+
+    def perform_create(self, serializer):
+        conversation = serializer.validated_data['conversation']
+        if self.request.user not in conversation.participants.all():
+            raise ValidationError("You are not a participant of this conversation.")
+        serializer.save(sender=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        """Custom create to return 201 with full message"""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
     def get_queryset(self):
         return Message.objects.filter(conversation__participants=self.request.user)
